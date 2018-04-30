e5ee91f4586a54916dbae65cedb59790d170c911
vking12 <vijayrajmahida@gmail.com>
2018-04-30 15:47:14 -0500

    distances ordered on table

--- server.R
+++ server.R
@@ -661,8 +661,28 @@ output$totalTornadoes <- renderDataTable(totalTornadoes, #extensions = 'Scroller
                                                 bFilter=0
                                               )
   )
+  
+  getOrdUnitsTable <- reactive({
+    df <- getUnitsTable()
+    vector <- NA
+    
+    if(!input$"metric") {
+      vector <- c(0, 10, 100, 120, 150, 20, 200, 240, 30, 50)
+    } else {
+      vector <- c(0, 16, 160, 193, 241, 32, 321, 386, 48, 80)
+    }
+    
+    ordData <- data.frame(
+      df,
+      lower = vector
+    )
+    
+    ordData <- ordData[order(ordData$lower),]
+    chosenData <- ordData[,c(1,2,3)]
+    chosenData
+  })
 
-  output$eDistanceTable<- renderDataTable(getUnitsTable(), extensions = 'Scroller',
+  output$eDistanceTable<- renderDataTable(getOrdUnitsTable(), extensions = 'Scroller',
                                                rownames = FALSE, options = list(
                                                  deferRender = TRUE,
                                                  scrollY = 600,
